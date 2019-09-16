9459669d3b06d2cc95c9c4b14dabe80affbe3f49
MacMini2012 <macmini2012@MacMini2012s-Mac-mini.local>
2019-09-16 17:30:15 +0700

    Structure_Classes

---
+++ 5_Structure_Classes/Structure.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class Resolution {
+    var width: Int
+    var height: Int
+    init(width: Int, height: Int) {
+        self.width = width
+        self.height = height
+    }
+}
+let hd = Resolution(width: 1920, height: 1080)
+var cinema = hd
+cinema.width = 2048
+print(hd.width)
+
