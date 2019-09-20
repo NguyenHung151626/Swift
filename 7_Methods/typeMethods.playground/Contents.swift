cbf3e8b34bf507de6e14be0b74834b3750d27443
MacMini2012 <macmini2012@MacMini2012s-Mac-mini.local>
2019-09-20 14:06:33 +0700

    methods

---
+++ 7_Methods/typeMethods.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+struct LevelTracker {
+    static var highestUnlockedLevel = 1
+    var currentLevel = 1
+    
+    static func unlock(_ level: Int) {
+        if level > highestUnlockedLevel { highestUnlockedLevel = level }
+    }
+}
+class Player {
+    let playerName: String
+    func complete(level: Int) {
+        LevelTracker.unlock(level + 1)
+    }
+    init(name: String) {
+        playerName = name
+    }
+}
+var player = Player(name: "Argyrios")
+player.complete(level: 1)
+print("highest unlocked level is now \(LevelTracker.highestUnlockedLevel)")
+// Prints "highest unlocked level is now 2"
